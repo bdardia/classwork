0a7357786c29eb8124b3fd05f10edeefbde7262e
BT_1N3_05 <BT_1N3_05@BT_1N3_05.bktech.local>
2017-09-28 15:29:20 -0400

    Chatbot.java stuff added

--- Nockles9/src/chatbot/Chatbot.java
+++ Nockles9/src/chatbot/Chatbot.java
@@ -2,8 +2,31 @@ package chatbot;
 
 public class Chatbot 
 {
+	private String username;
+	private boolean chatting;
+	private Topic ben;
+	
 	public Chatbot()
 	{
+		ben = new ChatbotBen();
+		username = "Unknown User";
+		chatting = true;
+	}
+
+	public void startChatting() 
+	{
+		ChatbotMain.print("I am a pizza chatbot. What's your name?");
+		username = ChatbotMain.getInput();
 		
+		while(chatting)
+		{
+			ChatbotMain.print("What's your pizza question?");
+			String response = ChatbotMain.getInput();
+			
+			if (ben.isTriggered(response))
+			{
+				chatting = false;
+			}
+		}
 	}
 }
