ac15775469c211b8113a9fc97c212c7be4f1fb07
BT_1N3_05 <BT_1N3_05@BT_1N3_05.bktech.local>
2017-10-19 15:22:34 -0400

    Added hobby, mingle, stateyourfriends, etc

--- Nockles9/src/arrays/Person.java
+++ Nockles9/src/arrays/Person.java
@@ -15,11 +15,15 @@ public class Person {
 	private String firstName;
 	private String lastName;
 	private Borough home;
+	private Hobby hobby;
+	private Person[] friends;
 	
 	public Person(String first, String last, Borough home) {
 		this.firstName = first;
 		this.lastName = last;
 		this.home = home;
+		this.friends = new Person[3];
+		this.hobby = Hobby.randomHobby();
 	}
 	
 	public String toString() {
@@ -27,4 +31,40 @@ public class Person {
 				lastName+" and I am from "+home+".";
 	}
 	
+	public void setInFirstPlace(Person f)
+	{
+		for (int i = friends.length - 1; i > 0; i -= 1)
+		{
+			friends[i] = friends[i - 1];
+		}
+		friends[0] = f;
+	}
+	
+	public void mingle(Person[] peers)
+	{
+		for (Person p: peers)
+		{
+			if (p != this)
+			{
+				setInFirstPlace(p);
+			}
+		}
+	}
+	
+	public void stateYourFriends()
+	{
+		String friendsList = "";
+		for (int i = 0; i < friends.length; i += 1)
+		{
+			if (i != (friends.length - 1))
+			{
+				friendsList += friends[i].firstName + ", ";
+			}
+			else
+			{
+				friendsList += "and " + friends[i].firstName;
+			}
+		}
+		System.out.println("My friends are " + friendsList);
+	}
 }
