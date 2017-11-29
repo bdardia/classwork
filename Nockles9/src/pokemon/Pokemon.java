24f66f03453a0f2081db010f1b073b3dabc6a289
BT_1N3_05 <BT_1N3_05@BT_1N3_05.bktech.local>
2017-11-29 15:04:33 -0500

    Init pokemon

---
+++ Nockles9/src/pokemon/Pokemon.java
@@ -0,0 +1,33 @@
+package pokemon;
+
+public class Pokemon 
+{
+	private int level;
+	private int hp;
+	private String name;
+	private boolean poisoned;
+	
+	public Pokemon(String name, int level)
+	{
+		this.name = name;
+		this.level = level;
+		this.hp = 100;
+		this.poisoned = false;
+	}
+	
+	public void iChooseYou()
+	{
+		System.out.println(this.name);
+		System.out.println(this.name);
+	}
+	
+	public int getHP()
+	{
+		return this.hp;
+	}
+	
+	public int setHP(int hp)
+	{
+		this.hp = hp;
+	}
+}
